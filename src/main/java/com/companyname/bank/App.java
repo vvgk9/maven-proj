6fa8f89d5b6e680e0d1c181d3b846f60154f0fad
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2019-08-19 11:41:36 +0530

    update app.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World..new modify!" );
+        System.out.println( "Hello World..!" );
     }
 }
